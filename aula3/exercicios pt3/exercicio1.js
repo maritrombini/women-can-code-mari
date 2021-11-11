4879d39d35a5b1822a23599d65f6418c4de0a844
maritrombini <marianagsena@gmail.com>
2021-11-10 22:11:59 -0300

    exercises

---
+++ aula3/exercicios pt3/exercicio1.js
@@ -0,0 +1,7 @@
+var hojeVaiChover = true
+
+if (hojeVaiChover == true) {
+  console.log('Leve seu guarda-chuva, hoje vai chover!')
+} else {
+  console.log('Fica susse, hoje será um dia de sol!')
+}
