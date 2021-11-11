72363e7c151669693ddedb470c1aa036dff22ca3
maritrombini <marianagsena@gmail.com>
2021-11-10 22:09:21 -0300

    exercises part II

---
+++ aula3/exercicios pt2/exercicio1.js
@@ -0,0 +1,9 @@
+var soma = 3 + 7
+var subtracao = 135 - 29
+var multiplicacao = 4 * 8
+var divisao = 225 / 5
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
