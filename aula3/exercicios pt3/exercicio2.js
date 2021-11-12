acb75271dee80e752b3a1fae0f40a1f30ca562c8
maritrombini <marianagsena@gmail.com>
2021-11-11 22:03:12 -0300

    including files

--- aula3/exercicios pt3/exercicio2.js
+++ aula3/exercicios pt3/exercicio2.js
@@ -12,7 +12,7 @@ var alternativaA = 5 > 1
 var alternativaB = 2000000 < 3000000000
 var alternativaC = 'Banana' == 'Banana'
 var alternativaD = '50' === 50
-var alternativaE = 'Carro' != 'bicicleta'
+var alternativaE = 'Carro' !== 'bicicleta'
 var alternativaF = 50 > 27
 var alternativaG = 975 <= 1200
 
